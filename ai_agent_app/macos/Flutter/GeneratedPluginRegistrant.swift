968d87f384c70c29cd783c4b2f49897ca6792ee3
80392083 <laizhenggui@oppo.com>
2026-01-27 11:22:08 +0800

    fix: add missing dependencies for app update feature
    - Add package_info_plus ^8.0.0 for getting app version info
    - Add path_provider ^2.1.4 for file path operations
    - Add open_file ^3.3.2 for APK installation
    
    Fixes remote build failure: 'package:package_info_plus/package_info_plus.dart' not found

--- ai_agent_app/macos/Flutter/GeneratedPluginRegistrant.swift
+++ ai_agent_app/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -9,6 +9,8 @@ import app_links
 import connectivity_plus
 import file_selector_macos
 import flutter_local_notifications
+import open_file_mac
+import package_info_plus
 import path_provider_foundation
 import shared_preferences_foundation
 import speech_to_text_macos
@@ -20,6 +22,8 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   ConnectivityPlugin.register(with: registry.registrar(forPlugin: "ConnectivityPlugin"))
   FileSelectorPlugin.register(with: registry.registrar(forPlugin: "FileSelectorPlugin"))
   FlutterLocalNotificationsPlugin.register(with: registry.registrar(forPlugin: "FlutterLocalNotificationsPlugin"))
+  OpenFilePlugin.register(with: registry.registrar(forPlugin: "OpenFilePlugin"))
+  FPPPackageInfoPlusPlugin.register(with: registry.registrar(forPlugin: "FPPPackageInfoPlusPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
   SpeechToTextMacosPlugin.register(with: registry.registrar(forPlugin: "SpeechToTextMacosPlugin"))
